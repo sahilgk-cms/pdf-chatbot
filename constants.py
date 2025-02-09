380014ba48272e8303263581218145cc0a474870
sahilgk-cms <sahil@catalysts.org>
2025-02-10 00:22:07 +0530

    constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+EMBEDDING_MODEL = "text-embedding-3-small"
+MODEL_NAME = "gpt-3.5-turbo"
+EMBED_DIMENSION = 512
+CHUNK_SIZE = 1024
+CHUNK_OVERLAP = 50
+NODE_THRESHOLD = 0.5
+WEAVIATE_URL = "https://td8hytwktcihmul5rlr8sa.c0.asia-southeast1.gcp.weaviate.cloud"
+WEAVIATE_INDEX = "BAT"
