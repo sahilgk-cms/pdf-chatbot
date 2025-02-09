ff3805d86f995ca367dc92a7729336aa2c5e0b31
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-02-10 00:33:31 +0530

    Update evaluate_chat.py

--- utils/evaluate_chat.py
+++ utils/evaluate_chat.py
@@ -11,11 +11,14 @@ import llama_index
 import nest_asyncio
 from constants import *
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 nest_asyncio.apply()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["openai"]["OPENAI_API_KEY"]
+
 if OPENAI_API_KEY:
     os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 else:
