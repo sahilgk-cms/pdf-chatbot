2976bbe7b174188487dde553d2b260376dcd098e
sahilgk-cms <sahil@catalysts.org>
2025-02-10 00:28:18 +0530

    updated chat app

--- chat_app.py
+++ chat_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
-from utils import load_vector_index_from_faiss, create_chat_engine, qa_chat
+import sys
+import os
+from constants import *
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+from utils.chat import *
+from utils.vector_embeddings import *
 
 
 st.title("Chatbot")
 
-faiss_path = "faiss_pdf_embeddings"
 
 if "chat_engine" not in st.session_state:
     st.session_state["chat_engine"] = None
 
 with st.spinner("Initialising chat engine...."):
     try:
-        vector_index = load_vector_index_from_faiss(file_path = faiss_path)
+        vector_index = load_vector_index_from_weaviate(weaviate_index = WEAVIATE_INDEX)
         chat_engine = create_chat_engine(vector_index)
         st.session_state["chat_engine"] = chat_engine
         st.success("Chat engine initialized successfully")
@@ -31,23 +35,24 @@ if st.session_state["chat_engine"]:
             answer, source_dict = qa_chat(chat_engine = st.session_state["chat_engine"],
                             query = query)
             
-            source_text = "\n\n**Sources:**\n"
-            for doc, pages in source_dict.items():
-                source_text += f"{doc}: (Pages: {', '.join(pages)})\n"
+            if answer and source_dict:
+                source_text = "\n\n**Sources:**\n"
+                for doc, pages in source_dict.items():
+                    source_text += f"{doc}: (Pages: {', '.join(pages)})\n"
 
-            full_response = f"{answer}{source_text}"
-            st.session_state["chat_history"].append({"role": "assistant", "message": full_response})
+                full_response = f"{answer}\n\n```{source_text}```"
+                st.session_state["chat_history"].append({"role": "assistant", "message": full_response})
+            else:
+                st.session_state["chat_history"].append({"role": "assistant",
+                                                            "message": "Answer not found in any of the documents"})
 
         except Exception as e:
-            st.error(f"Error: {e}")
+             st.error(f"Error: {e}")
 
     for chat in st.session_state["chat_history"]:
         if chat["role"] == "user":
             st.chat_message("user").write(f"**Query:** {chat["message"]}")
         elif chat["role"] == "assistant":
             st.chat_message("assistant").write(f"**Answer:** {chat["message"]}")
-        # elif chat["role"] == "sources":
-        #     st.chat_message("assistant").write(f"**Sources:** ")
-        #     for doc, pages in chat["message"].items():
-        #         st.write(f"{doc}: (Pages: {', '.join(pages)})")
+      
     
