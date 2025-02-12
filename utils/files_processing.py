27a69be56f65384d0722de08dfb32834e55723c5
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-02-12 14:45:05 +0530

    Update files_processing.py

--- utils/files_processing.py
+++ utils/files_processing.py
@@ -5,8 +5,10 @@ from llama_index.core import  Document
 from typing import List
 import os
 import llama_index
+from pypdf import PdfReader
 from dotenv import load_dotenv
 
+############################################## LLAMAINDEX DOCUMENTS ################################################################
 
 def convert_pdf_files_into_llamaindex_docs(folder_path: str,
                                            chunk_size: int,
@@ -36,3 +38,50 @@ def convert_pdf_files_into_llamaindex_docs(folder_path: str,
         llamaindex_documents.append(llamaindex_doc)
     
     return llamaindex_documents
+
+################################################ PDF TO TEXT ################################################################################
+
+def convert_pdf_into_text(folder_path: str) -> str:
+  '''
+  This function converts all pdf data into text string
+  Args:
+    folder path containing all pdfs
+  Returns:
+    text string
+  '''
+  pdf_files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
+  print(f"PDF Files in Google Drive Folder: {pdf_files}")
+
+  text = ""
+  for pdf_file in pdf_files:
+    pdf_path = os.path.join(folder_path, pdf_file)
+    reader = PdfReader(pdf_path)
+
+    for page in reader.pages:
+      text += page.extract_text()
+
+  return text
+
+def save_string_to_textfile(text: str, filepath: str):
+  '''
+  This function saves string to text file
+  Args:
+    string
+  Returns:
+    None
+  '''
+  with open(filepath, "w", encoding = "utf-8") as f:
+    f.write(text)
+
+
+def load_string_from_textfile(filepath: str) -> str:
+  '''
+  This function loads string from a text file
+  Args:
+    Text file path
+  Returns:
+    String
+  '''
+  with open(filepath, "r", encoding="utf-8") as f:
+    text = f.read()
+  return text
