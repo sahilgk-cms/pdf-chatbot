6deca8df8ae8455cdcdefe62af815a393bc32f28
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-02-17 10:01:42 +0530

    Update files_processing.py

--- utils/files_processing.py
+++ utils/files_processing.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from llama_index.core import  Document
