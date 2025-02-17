e744ff100ad1f0fb99d740a41377d89d73651802
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-02-17 09:58:33 +0530

    Update files_processing.py

--- utils/files_processing.py
+++ utils/files_processing.py
@@ -85,3 +85,85 @@ def load_string_from_textfile(filepath: str) -> str:
   with open(filepath, "r", encoding="utf-8") as f:
     text = f.read()
   return text
+
+
+################################################ EXCEL TO JSON ################################################################################
+
+def get_single_sheet_inside_excel(file_path: str, sheet_name: str):
+  '''
+  This function returns the dataframe from execl file & a given sheet
+  Args:
+    file path, sheet name
+  Returns:
+    dataframe of that given sheet
+    '''
+  df = pd.read_excel(file_path, sheet_name)
+  columns_to_be_dropped = [col for col in df.columns if col.startswith("Unnamed")]
+  df = df.drop(columns_to_be_dropped, axis = 1)
+  return df
+
+
+def convert_dataframe_into_dict_and_llamaindex_docs(file_path: str,
+                                                    sheet_name: str) -> Tuple[List[dict], List[llama_index.core.schema.Document]]:
+    '''
+    This function converts the single sheet inside excel file into dictionary records (for chat prompt) & llamaindex documents (for embeddings)
+    Args:
+      file path, sheet name
+    Returns:
+      dictionary records (for chat prompt) & llamaindex documents (for embeddings)
+    '''
+    df = get_single_sheet_inside_excel(file_path, sheet_name)
+
+    #this is for Gemini prompt
+    dict_ = df.to_dict(orient = "records")
+
+    #this is for LlamaIndex documents & OpenAI
+    documents = []
+    for entry in dict_:
+      cleaned_entry = {k.strip(): v.strip() if isinstance(v, str) else v for k, v in entry.items()}
+      text = "\n".join(f"{key}:{value}" for key, value in cleaned_entry.items())
+      doc = Document(text=text)
+      doc.metadata = {"file_name": os.path.basename(file_path),
+                     "sheet": sheet_name}
+      documents.append(doc)
+
+    return dict_, documents
+
+
+def custom_serializer(obj):
+    """
+    Serialize non-JSON serializable objects, such as datetime.
+    Args:
+        obj (any): The object to serialize.
+    Returns:
+        str: An ISO 8601 formatted string if the object is a datetime instance.
+    Raises:
+        TypeError: If the object type is not supported for serialization.
+    """
+    if isinstance(obj, datetime):
+      return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} is not serializable")
+
+
+def save_dict_to_json(input_dict: dict, file_path: str):
+  '''
+  This function saves the dictionary to JSON file
+  Args:
+    input dictionary, file path
+  Returns:
+    None
+  '''
+  with open(file_path, "w") as f:
+    json.dump(input_dict, f, default=custom_serializer)
+
+def load_dict_from_json(file_path: str) -> dict:
+  '''
+  This function loads the dictionary from json file
+  Args:
+    file path
+  Returns:
+    dictionary
+  '''
+  with open(file_path, "r") as f:
+    loaded_dict  = json.load(f)
+  return loaded_dict
